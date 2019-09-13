51a4cf2912cdd4e9fe5df2299495e64aed71eb22
midterm <ksultana102@gmail.com>
2019-09-13 00:00:25 -0400

    update

--- src/math/problems/UnitTestingMath.java
+++ src/math/problems/UnitTestingMath.java
@@ -1,3 +1,4 @@
+/*
 package math.problems;
 
 import design.EmployeeInfo;
@@ -8,16 +9,19 @@ import static org.testng.AssertJUnit.assertEquals;
 
 public class UnitTestingMath {
 
-        //Apply Unit testing into each classes and methods in this package.
-        @Test
-        public void Test() {
-            Factorial factorial = new Factorial();
+   */
+/* //Apply Unit testing into each classes and methods in this package.
+    @Test
+    public void Test() {
+        Factorial factorial = new Factorial();
 
-            factorial.factorial(4);
+        factorial.factorial(4);
 
-            Assert.assertEquals(24,factorial.factorial(4));
-            Fibonacci fibonacci=new Fibonacci();
-            public void fibonacci_iterative() {
-                Assert.assertEquals(34, fibonacci(8));
+        Assert.assertEquals(24, factorial.factorial(4));
+        Fibonacci fibonacci = new Fibonacci();
+        public void fibonacci_iterative () {
+            Assert.assertEquals(34, fibonacci);
 
-            }
+        }
+    }
+}*/
