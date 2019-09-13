e1cee341fc73a2f990cf0ee95a7e29215d6548a8
midterm <ksultana102@gmail.com>
2019-09-12 22:59:08 -0400

    Last Commit

--- src/math/problems/UnitTestingMath.java
+++ src/math/problems/UnitTestingMath.java
@@ -4,6 +4,8 @@ import design.EmployeeInfo;
 import org.junit.Assert;
 import org.junit.Test;
 
+import static org.testng.AssertJUnit.assertEquals;
+
 public class UnitTestingMath {
 
         //Apply Unit testing into each classes and methods in this package.
@@ -14,7 +16,8 @@ public class UnitTestingMath {
             factorial.factorial(4);
 
             Assert.assertEquals(24,factorial.factorial(4));
+            Fibonacci fibonacci=new Fibonacci();
+            public void fibonacci_iterative() {
+                Assert.assertEquals(34, fibonacci(8));
 
-        }
-    }
-
+            }
